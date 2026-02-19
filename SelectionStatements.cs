5aa353ba26c72a58401b1fe0c0ef3438edd877ee
ST10483371 <st10483371@rcconnect.edu.za>
2026-02-19 15:12:31 +0200

    Update SelectionStatements.cs

--- SelectionStatements.cs
+++ SelectionStatements.cs
@@ -28,9 +28,19 @@ namespace HelloWorld
 	    
 	    //if statements
 	    Console.WriteLine("Enter the number that corresponds with the day of the week ");
-	    int day = Convert.ToInt32(Console.ReadLine()); //converts input to integer
-	    int day2 = int.Parse(Console.ReadLine());
+	    //int day = Convert.ToInt32(Console.ReadLine()); //converts input to integer
+	    int day = int.Parse(Console.ReadLine()); //converts 
+	    // if (condition is true){}
+	    if (day == 1){ Console.WriteLine("Monday"); }
+	    if (day == 2){ Console.WriteLine("Tuesday"); }
+	    if (day == 3){ Console.WriteLine("Wednesday"); }
+	    if (day == 4){ Console.WriteLine("Thursday"); }
+	    if (day == 5){ Console.WriteLine("Friday"); }
+	    if (day == 6){ Console.WriteLine("Saturday"); }
+	    if (day == 7){ Console.WriteLine("Sunday"); }
+	    
 	    
 			
 		}
 	}
+}
